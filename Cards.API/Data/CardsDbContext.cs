71dd3d7e1ed1f82bd2972b6df23e36df991f7036
Thiago Pessoa <thiagokxl5@gmail.com>
2022-08-01 22:36:50 -0300

    CardsDbContext

---
+++ Cards.API/Data/CardsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cards.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
